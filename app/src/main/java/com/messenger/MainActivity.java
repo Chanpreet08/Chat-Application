7ae1ccb29c30a8cb52dbe91033513fa19a1cab93
Chanpreet08 <chanpreet.chhabra@gmail.com>
2017-04-07 18:54:00 +0530

    pre final app

--- app/src/main/java/com/messenger/MainActivity.java
+++ app/src/main/java/com/messenger/MainActivity.java
@@ -1,24 +1,40 @@
 package com.messenger;
 
 import android.content.Intent;
+import android.icu.text.DateFormat;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FloatingActionButton fab;
+    private FirebaseListAdapter<Message> adapter;
+    private ListView messageList;
+    private TextView messageText;
+    private TextView messageUser;
+    private TextView messageTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fab =(FloatingActionButton) findViewById(R.id.fab);
+        messageList=(ListView) findViewById(R.id.list_of_messages);
 
         if(FirebaseAuth.getInstance().getCurrentUser()==null)
         {
@@ -26,14 +42,36 @@ public class MainActivity extends AppCompatActivity {
         }
         else
         {
-            Toast.makeText(MainActivity.this,"Welcome"+FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this,"Welcome "+FirebaseAuth.getInstance().getCurrentUser().getDisplayName(),Toast.LENGTH_LONG).show();
             displayChatMessage();
         }
+
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText input = (EditText) findViewById(R.id.input);
+                FirebaseDatabase.getInstance().getReference().push().setValue( new Message(input.getText().toString(),FirebaseAuth.getInstance().getCurrentUser().getDisplayName()));
+                input.setText("");
+            }
+        });
     }
 
     private void displayChatMessage()
     {
+        adapter = new FirebaseListAdapter<Message>(this,Message.class,R.layout.message,FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, Message model, int position) {
+                messageText = (TextView)v.findViewById(R.id.message_text);
+                messageUser = (TextView)v.findViewById(R.id.message_user);
+                messageTime = (TextView)v.findViewById(R.id.message_time);
 
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+                messageTime.setText(android.text.format.DateFormat.format("dd-MM-yyyy (HH:mm:ss)", model.getMessageTime()));
+            }
+        };
+        messageList.setAdapter(adapter);
     }
 
     @Override
